b273daa301fba0219ff1aef2e97bb7915c12a28b
tonoaryanto <tonoaryanto@gmail.com>
2021-01-02 10:31:55 +0700

    connect program0

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,7 +5,13 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		echo $this->input->get("convo_id");
-		echo $this->input->get("query");
+		$identitas = $this->input->get("convo_id");
+		$pesan = $this->input->get("query");
+
+		$getcontent = file_get_contents("https://botchat.anselljaya.com/chatbot/conversation_star.php?say=$pesan&convo_id=$identitas");
+
+		$hasil = json_decode($getcontent,true);
+
+		echo $hasil["botsay"];
 	}
 }
