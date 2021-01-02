ee1f1bdc776d06fb19c6d782e264b47ef2efcf31
tonoaryanto <tonoaryanto@gmail.com>
2021-01-02 18:29:00 +0700

    jarvis delay chat 3h

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,8 +18,6 @@ class Welcome extends CI_Controller {
 			$identitas .= $extr2[$i];
 		}
 
-		$ceklast = $this->db->query("SELECT kontak,tanggalwaktu FROM log_user WHERE kontak = '".$identitas."'")->num_rows();
-
 		$namabot = $this->db->query("SELECT nama_bot FROM data_bot WHERE keterangan = 'aktif'")->row_array()['nama_bot'];
 
 		$expesan = explode(" ",$pesan);
@@ -41,37 +39,43 @@ class Welcome extends CI_Controller {
 		$panggilsakit = $sakit[rand(0,(count($sakit) - 1))];
 
 		if(strtolower($expesan[0]) == $namabot){
-			$hi = "If you want to chat with me, please type my name followed by the following command: \r\n weather/cuaca \r\n \r\n Example: ".ucwords($namabot)." weather";
+			$hi = "If you want to chat with me, please type my name followed by the following command: \r\n- weather/cuaca \r\n \r\nExample: ".ucwords($namabot)." weather";
 			if(isset($expesan[1])){
 				$perintah = strtolower($expesan[1]);
 				if($perintah == 'cuaca' or $perintah == 'weather'){
-					$infocuaca = "To find out the weather where you are currently, please type the following command : \r\n ".ucwords($namabot)." weather in <location name> \r\n or \r\n ".ucwords($namabot)." cuaca di <nama lokasi>";
+					$infocuaca = "To find out the weather where you are currently, please type the following command :\r\n".ucwords($namabot)." weather in <location name> \r\nor \r\n".ucwords($namabot)." cuaca di <nama lokasi>";
 					if(isset($expesan[2]) and isset($expesan[3])){
 						$petunjuk = strtolower($expesan[2]);
-						$lokasi = strtolower($expesan[3]);						
+						$lokasi = strtolower($expesan[3]);
+						if(isset($expesan[4])){
+							for ($i=3; $i < count($expesan); $i++) {
+								$lokasi .= ' '.$expesan[$i];
+							}
+						}
 						if($petunjuk == 'in' or $petunjuk == 'di'){
-							$rdtcuaca = file_get_contents("https://api.weatherapi.com/v1/current.json?key=80aff1b16f50451babc90054210201&q=".$lokasi);
+							$rdtcuaca = @file_get_contents("https://api.weatherapi.com/v1/current.json?key=80aff1b16f50451babc90054210201&q=".$lokasi);
+							print_r($rdtcuaca);
 							$dtcuaca =  json_decode($rdtcuaca);
 
-							$dtlokasi =  $dtcuaca->{'location'};
-							if(isset($dtlokasi)){
+							if(isset($dtcuaca->{'location'})){
+								$dtlokasi =  $dtcuaca->{'location'};
 								$dtsaatini =  $dtcuaca->{'current'};
 								$dtshow = "Name : ".$dtlokasi->{'name'};
 
-								$dtshow .= " \r\n Region : ".$dtlokasi->{'region'};
-								$dtshow .= " \r\n Country : ".$dtlokasi->{'country'};
-								$dtshow .= " \r\n Localtime : ".date_format(date_create($dtlokasi->{'localtime'}),"H:i:s d-m-Y");
-								$dtshow .= " \r\n Condition : ".$dtsaatini->{'condition'}->{'text'};
-								$dtshow .= " \r\n Temperature : ".$dtsaatini->{'temp_c'}. " C";
-								$dtshow .= " \r\n Humidity : ".$dtsaatini->{'humidity'}." %";
-								$dtshow .= " \r\n Wind : ".$dtsaatini->{'wind_kph'}." km/h";
+								$dtshow .= " \r\nRegion : ".$dtlokasi->{'region'};
+								$dtshow .= " \r\nCountry : ".$dtlokasi->{'country'};
+								$dtshow .= " \r\nLocaltime : ".date_format(date_create($dtlokasi->{'localtime'}),"H:i:s d-m-Y");
+								$dtshow .= " \r\nCondition : ".$dtsaatini->{'condition'}->{'text'};
+								$dtshow .= " \r\nTemperature : ".$dtsaatini->{'temp_c'}. " C";
+								$dtshow .= " \r\nHumidity : ".$dtsaatini->{'humidity'}." %";
+								$dtshow .= " \r\nWind : ".$dtsaatini->{'wind_kph'}." km/h";
 
 								echo $dtshow;
 							}else{
 								echo "Oops your location is unknown.";
 							}
 						}else{
-							echo $panggilsakit." \r\n ".$infocuaca;
+							echo $panggilsakit." \r\n".$infocuaca;
 						}
 					}else{
 						echo $infocuaca;
@@ -83,7 +87,31 @@ class Welcome extends CI_Controller {
 				echo "Hi. ".$hi;
 			}
 		}else{
-			echo "Hello, I'm ".ucwords($namabot).". I am an Artificial Intelligence. Please wait a moment my master will reply to your chat or you can say *".ucwords($namabot)."* to chat with me.";
+			$pesanini = "Hello, I'm ".ucwords($namabot).". I am an Artificial Intelligence. Please wait a moment my master will reply to your chat or you can say *".ucwords($namabot)."* to chat with me.";
+
+			$cek = $this->db->query("SELECT kontak,tanggalwaktu FROM log_user WHERE kontak = '".$identitas."'");
+			$ceklast = $cek->num_rows();
+
+			if($ceklast > 0){
+				$cekdtsimpan = $cek->row_array();
+				$harisimpan = date_format(date_create($cekdtsimpan['tanggalwaktu']), "H");
+				$harini = date("H");
+				$hinterval = (int)$harini - (int)$harisimpan;
+				if($hinterval >= 3){
+					$data['tanggalwaktu'] = date("Y-m-d H:i:s");
+	
+					$this->db->where(['kontak'=>$identitas]);
+					$this->db->update("log_user", $data);
+
+					echo $pesanini;
+				}else{return;}
+			}else{
+				$data['kontak'] = $identitas;
+				$data['tanggalwaktu'] = date("Y-m-d H:i:s");
+
+				$this->db->insert("log_user", $data);
+				echo $pesanini;
+			}
 		}
 	}
 }
